3bf9da7081cf540b300dc6964de046d5eeb82e21
Ramazan KAYIŞ <kayis.ramazan@gmail.com>
2025-05-02 14:50:01 +0300

    updated cartModal

---
+++ src/components/store/UserProgressContext.jsx
@@ -0,0 +1,41 @@
+import { createContext, useState } from "react";
+
+const UserProgressContext = createContext({
+  progress: "", // cart,checkout
+  showCart: () => {},
+  hideCart: () => {},
+  showCheckout: () => {},
+  hideCheckout: () => {},
+});
+
+export const UserProgressContextProvider = ({ children }) => {
+  const [userProgress, setUserProgress] = useState("");
+  const showCart = () => {
+    setUserProgress("cart");
+  };
+  const shideCart = () => {
+    setUserProgress("");
+  };
+  const showCheckout = () => {
+    setUserProgress("checkout");
+  };
+  const hideCheckout = () => {
+    setUserProgress("");
+  };
+
+  const userProgressCtx = {
+    progress: userProgress,
+    showCart,
+    shideCart,
+    showCheckout,
+    hideCheckout,
+  };
+
+  return (
+    <UserProgressContext.Provider value={userProgressCtx}>
+      {children}
+    </UserProgressContext.Provider>
+  );
+};
+
+export default UserProgressContext;
